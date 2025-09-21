8e4888699b1c5074159e42542ab56d0ecf9a67df
zc00963 <zc00963@ES-TCCT-PF4AV9WY>
2025-09-21 09:50:58 +0200

    Cambios en el classpath por inlcuir las clases en repositorio git

--- 2-DAM-PSP_local/src/edu/thepower/psp/u1procesosservicios/U1E02EjecutarProcesoJava.java
+++ 2-DAM-PSP_local/src/edu/thepower/psp/u1procesosservicios/U1E02EjecutarProcesoJava.java
@@ -14,7 +14,8 @@ public class U1E02EjecutarProcesoJava {
 
 	public void lanzarSumador (int n1, int n2) {
 		String clase = "edu.thepower.psp.u1procesosservicios.U1E02Sumador";
-		String classpath = "C:\\Users\\zc00963\\eclipse-workspace\\2-DAM-PSP_local\\bin";
+		//String classpath = "C:\\Users\\zc00963\\eclipse-workspace\\2-DAM-PSP_local\\bin";
+		String classpath = "C:\\Users\\zc00963\\git\\2-DAM-Temario-backup\\2-DAM-PSP_local\\bin";
 		//String jre = "C:\\Users\\zc00963\\.p2\\pool\\plugins\\org.eclipse.justj.openjdk.hotspot.jre.full.win32.x86_64_21.0.5.v20241023-1957\\jre\\bin\\java.exe";
 		
 		try {
@@ -26,32 +27,49 @@ public class U1E02EjecutarProcesoJava {
 			// Es necesario leer la salida del proceso. En caso contrario no se obtendrá nada en la ejecución. 
 			// Leer la salida del proceso, volcar a la consola estándar.
 			Process p = pb.start();
-			BufferedReader br = new BufferedReader (new InputStreamReader(p.getInputStream()));
+			BufferedReader out = new BufferedReader (new InputStreamReader(p.getInputStream()));
 			String linea;
-			while ((linea = br.readLine()) != null)
+			while ((linea = out.readLine()) != null)
 				System.out.println(linea);
 			
 			// Leer errores del proceso, si aparecen. Volcar a la consola de error.
-			br = new BufferedReader (new InputStreamReader(p.getErrorStream()));
-			while ((linea = br.readLine()) != null)
+			BufferedReader err = new BufferedReader (new InputStreamReader(p.getErrorStream()));
+			while ((linea = err.readLine()) != null)
 				System.err.println(linea);
 				
 			p.waitFor(); // Se espera a que el proceso finalice.
 			*/
 			
 			/*
-			// 2. Recuperar rsultado y errores de la ejecución con inheritIO (o redirect)
-			pb.inheritIO();
-			// *** Alternativamente
-			// pb.redirectOutput(ProcessBuilder.Redirect.INHERIT);
-			// pb.redirectError(ProcessBuilder.Redirect.INHERIT);
-			pb.start();
+			// 2. Mismo caso que en el apartado anterior pero uniendo salida y errores en un solo stream.
+			Process p = pb.start();
+			pb.redirectErrorStream(true);
+			BufferedReader br = new BufferedReader (new InputStreamReader(p.getInputStream()));
+			String linea;
+			while ((linea = br.readLine()) != null)
+				System.out.println(linea);
+			
+			p.waitFor(); // Se espera a que el proceso finalice.
 			*/
 			
-			// 3. Recuperar rsultado y errores de la ejecución y redirigir a fichero
+			/*
+			// 3. Recuperar resultado y errores de la ejecución con inheritIO (o redirect)
+			//  Salida y errores se redirigen salida por consola de la clase padre Java.
+			// pb.inheritIO();
+			// *** Alternativamente
+			/
+			 */
+			pb.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+			pb.redirectError(ProcessBuilder.Redirect.INHERIT);
+			Process p = pb.start();
+			//p.waitFor();
+			
+			// 4. Recuperar rsultado y errores de la ejecución y redirigir a fichero
+			/*
 			pb.redirectOutput(new File("salida_" + n1 + "-" + n2 + ".txt"));
 			pb.redirectError(new File("error_" + n1 + "-" + n2 + ".txt"));
 			pb.start();
+			*/
 		} catch (Exception e) {
 			System.out.println("Error al ejecutar el sumador: " + e.getMessage());
 			e.printStackTrace();

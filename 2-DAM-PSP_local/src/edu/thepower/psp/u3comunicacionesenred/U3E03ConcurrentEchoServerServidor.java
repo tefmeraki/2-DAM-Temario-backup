1c27359b99714b3b16c89c788516c542725f56b2
zc00963 <zc00963@ES-TCCT-PF4AV9WY>
2025-11-10 10:15:23 +0100

    Commit clase tema 3

---
+++ 2-DAM-PSP_local/src/edu/thepower/psp/u3comunicacionesenred/U3E03ConcurrentEchoServerServidor.java
@@ -0,0 +1,63 @@
+package edu.thepower.psp.u3comunicacionesenred;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+class GestorCliente implements Runnable {
+	Socket socket;
+	String nombre;
+	
+	public GestorCliente (Socket socket) {
+		this.socket = socket;
+	}
+	
+	@Override
+	public void run() {
+		// Obtenemos el nombre del thread
+		this.nombre = "[" + Thread.currentThread().getName() + "] ";
+		
+		try {
+			System.out.println(nombre + "Solicitud atendida por " + nombre + " en el puerto " + socket.getLocalPort());
+			InputStream is = socket.getInputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			OutputStream os = socket.getOutputStream();
+			PrintWriter writer = new PrintWriter(os, true);
+			String linea;
+			while ((linea = br.readLine()) != null) {
+				System.out.println(nombre + "Recibido de cliente: " + linea);
+				writer.println(linea.toLowerCase());
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println(nombre + "*** Finalizada la conexión por el cliente.");
+	}
+}
+
+public class U3E03ConcurrentEchoServerServidor {
+
+	public static void main(String[] args) {
+		final int PUERTO = 5000;
+		
+		System.out.println("Esperando conexiones en el puerto " + PUERTO);
+		try (ServerSocket servidor = new ServerSocket(PUERTO)){
+			while (true) {
+				Thread t = new Thread(new GestorCliente(servidor.accept()));
+				System.out.println("[Servidor] Recibida solicitud de conexión de nuevo cliente.");
+				t.start();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
